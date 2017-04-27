0fca6e4281d3259d929803efd8974ad2455b530b
Nicolas Machado <nicoma63@gmail.com>
2017-04-27 14:14:23 +0200

    React first implementation

--- src/App.js
+++ src/App.js
@@ -1,18 +1,52 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+
+      	<header>
+              <div className="burger-container">
+                  <i className="fa fa-bars fa-lg"></i>
+              </div>
+              <h1>Picture That!</h1>
+      	</header>
+
+    	<main>
+
+        <div className="container col1">
+			<div className="story-preview inside-cont">
+                <div className="story-thumbnail-container">
+                    <a href="story.html"><img className="story-thumbnail" src={require('./images/story.jpg')} alt="Thumbnail" width="100%" /></a>
+                </div>
+                <h2 className="story-preview-title">How my colony got wiped out</h2>
+                <p className="story-preview">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+                tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
+                vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
+                no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+                <div className="story-preview-date">
+                    <i className="fa fa-user" aria-hidden="true"></i> Johnny | <i className="fa fa-clock-o" aria-hidden="true"></i> 2 hours ago | 3 comments
+                </div>
+			</div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <div className="container col1">
+			<div className="story-preview inside-cont">
+                <div className="story-thumbnail">
+                    <a href="story.html"><img src={require('./images/story2.jpg')} alt="Thumbnail" width="100%" /></a>
+                </div>
+                <h2 className="story-preview-title">The perfect base</h2>
+                <p className="story-preview">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+                tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
+                vero eos et accusam et justo duo dolores et ea rebum.</p>
+                <div className="story-preview-info">
+                    <i className="fa fa-user" aria-hidden="true"></i> Nick | <i className="fa fa-clock-o" aria-hidden="true"></i> 3 days ago | 1 comment
+                </div>
+			</div>
+        </div>
+
+    	</main>
+
       </div>
     );
   }
