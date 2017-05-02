816d2e19611338662ad609bece29d6f8b48368e6
Nicolas Machado <nicoma63@gmail.com>
2017-05-02 15:53:17 +0200

    Fixed Router

--- src/components/burger.js
+++ src/components/burger.js
@@ -1,27 +1,28 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import {API_URL} from '../config.js';
+import {Link} from 'react-router-dom';
+import {toggleBurger} from '../actions';
 
 export class Burger extends React.Component {
     render() {
-
         const links = this.props.currentUser.id ?
         <div>
-            <a href="/"><div className="burger-button">Home</div></a>
-            <a href={'/profile/' + this.props.currentUser.id}><div className="burger-button">Profile</div></a>
-            <a href="/new-story"><div className="burger-button">New Story</div></a>
-            <a href={API_URL + '/auth/logout'}><div className="burger-button">Log out</div></a>
+            <Link to="/"><div className="burger-button">Home</div></Link>
+            <Link to={'/profile/' + this.props.currentUser.id} ><div className="burger-button">Profile</div></Link>
+            <Link to="/new-story"><div className="burger-button">New Story</div></Link>
+            <Link to={API_URL + '/auth/logout'}><div className="burger-button">Log out</div></Link>
         </div>
         :
         <div>
-            <a href="/"><div className="burger-button">Home</div></a>
-            <a href="/login"><div className="burger-button">Log in</div></a>
+            <Link to="/"><div className="burger-button">Home</div></Link>
+            <Link to="/login"><div className="burger-button">Log in</div></Link>
         </div>
         ;
 
         return (
             <div className="burger">
-                  <div className="burger-menu">
+                  <div className="burger-menu" onClick={e => this.props.dispatch(toggleBurger())}>
                         {links}
                   </div>
             </div>
