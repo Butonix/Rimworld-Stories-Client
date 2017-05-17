5a499f513f6e2084928ee9b08e26ed8df58019d5
Nicolas Machado <nicoma63@gmail.com>
2017-05-17 12:20:58 +0200

    changed heroku name

--- src/config.js
+++ src/config.js
@@ -1,1 +1,1 @@
-export const API_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:8080' : 'https://fathomless-chamber-86254.herokuapp.com';
+export const API_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:8080' : 'https://rimworld-stories.herokuapp.com';
